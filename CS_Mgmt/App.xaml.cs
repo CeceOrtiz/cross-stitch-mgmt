bcb7e47e0d6212297f0a305ce18d5dc762c9f8e9
CeceOrtiz <cecemortiz@gmail.com>
2023-07-27 10:11:57 -0400

    Added db check to app.xaml.cs

--- CS_Mgmt/App.xaml.cs
+++ CS_Mgmt/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using SQLite;
+using CS_Mgmt.Models;
 
 namespace CS_Mgmt
 {
@@ -13,5 +16,32 @@ namespace CS_Mgmt
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            string dbFilename = "cs_mgmt_db.db";
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), dbFilename);
+
+            // Create db if it doesn't exist already
+            if (!File.Exists(dbPath))
+            {
+                using (var dbContext = new SQLiteConnection(dbPath))
+                {
+                    // Enable foreign keys
+                    dbContext.Execute("PRAGMA foreign_keys = ON;");
+
+                    dbContext.CreateTable<Floss>();
+                    dbContext.CreateTable<Pattern>();
+                    dbContext.CreateTable<PatternFloss>();
+                    dbContext.CreateTable<UserFloss>();
+                    dbContext.CreateTable<Fabric>();
+                    dbContext.CreateTable<Supply>();
+
+
+                }
+
+            }
+        }
     }
 }
