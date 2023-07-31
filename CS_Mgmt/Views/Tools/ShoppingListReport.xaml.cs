81e39ba5ebadc8c90058d64100fbba19aae8a18f
CeceOrtiz <cecemortiz@gmail.com>
2023-07-31 08:45:05 -0400

    Design outline for all Add pages.

---
+++ CS_Mgmt/Views/Tools/ShoppingListReport.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_Mgmt.Views.Tools
+{
+    /// <summary>
+    /// Interaction logic for ShoppingListReport.xaml
+    /// </summary>
+    public partial class ShoppingListReport : Page
+    {
+        public ShoppingListReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
