798be062eca7c62f7de428a78e175a3c6bd4f7f4
CeceOrtiz <cecemortiz@gmail.com>
2023-08-09 09:02:03 -0400

    Added ViewModels folder, ViewModelBase, and AddFlossVM to start working with MVVM.

---
+++ CS_Mgmt/ViewModels/AddFlossVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS_Mgmt.Models;
+
+namespace CS_Mgmt.ViewModels
+{
+    internal class AddFlossVM : ViewModelBase
+    {
+        public ObservableCollection<Floss> NonUserFloss { get; } = new ObservableCollection<Floss>();
+        public Floss SelectedFloss { get; set; }
+        public int Quantity { get; set; }
+        public string StorageLocation { get; set; }
+    }
+}
