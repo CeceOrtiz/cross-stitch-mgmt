9a3d05b0740c2cd75940ba72a72ece949a2ef23c
CeceOrtiz <cecemortiz@gmail.com>
2023-08-29 10:29:55 -0400

    Created inventory report page and began adding UI elements.

---
+++ CS_Mgmt/Views/ToolViews/InventoryReport.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CS_Mgmt.Views.Dashboard;
+using OfficeOpenXml;
+
+namespace CS_Mgmt.Views.ToolViews
+{
+    /// <summary>
+    /// Interaction logic for InventoryReport.xaml
+    /// </summary>
+    public partial class InventoryReport : Page
+    {
+        public InventoryReport()
+        {
+            InitializeComponent();
+        }
+
+        // Page Loaded
+
+        // Event handlers for double-clicking DGs
+        private void PatternsDG_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        // Classes for various DG items
+
+        // Save to Excel
+
+
+
+
+        private void Return_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow.MainFrame.NavigationService.Navigate(new Dash());
+        }
+        
+    }
+}
